b44c0001430702e7d4d100e9e42a369b6e4278bb
Atticus Carter <55251749+atticus-carter@users.noreply.github.com>
2025-02-15 08:16:26 -0800

    Update dataview.py

--- pages/dataview.py
+++ pages/dataview.py
@@ -4,10 +4,10 @@ import pandas as pd
 import re
 import os
 from scripts.model_generation import model_urls # Replace with your path
-import json
-from google.oauth2 import service_account
 import io
 import cv2 # opencv
+import boto3  # Import boto3
+from google.oauth2 import service_account
 
 # --- Camera Names ---
 camera_names = ["PC01A_CAMDSC102", "LV01C_CAMDSB106", "MJ01C_CAMDSB107", "MJ01B_CAMDSB103"]
@@ -25,46 +25,28 @@ else:
     year_month = "2021-08"  # The year and month of the data
     #try:
         # Load credentials from secrets to cloud storage
-    gcs = st.secrets["connections.gcs"]
-
-        # Make your credential in json, its okay to expose everything as nothing is loaded.
-       # except:
-       # st.write("Missing credential. Make sure you configure your secrets manager.")
-       # st.stop()
-        # Authenticate into google
-    con = duckdb.connect(database=':memory:', read_only=False)
-    csv_filepath = f"{camera_id}_data_{year_month}/predictions.parquet"
-
-    """
-    if  not os.path.exists(csv_filepath):
-        st.write("Please ensure that this session has data for the GCS")
-        st.write("You can configure more data from the homepage of this session")
-        st.stop()
-        #st.csv will not work on links unless it is public
-    else:"""
-    st.write("Configuring google connect... Please wait!")
     try:
-        df = pd.read_parquet(csv_filepath)
+        # Create an S3 client
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=st.secrets["connections.s3"]["aws_access_key_id"],
+            aws_secret_access_key=st.secrets["connections.s3"]["aws_secret_access_key"],
+            region_name=st.secrets["connections.s3"]["region_name"],
+        )
 
-        st.header("Test Pandas: Successfully connected to GCS file")
-        st.subheader("Here is the data in pandas")
+        # Bucket connection test
+        #response = s3_client.list_buckets()
+        #buckets = [bucket["Name"] for bucket in response["Buckets"]]
+        #st.write(f"AWS, the buckets are: {buckets}")
 
-        #if this is true,
-        st.dataframe(df)
-    except:
-        st.write("Please test the code first! If this does not work, then ensure that you have data for the camera folder")
+    except Exception as e:
+        st.error(f"The code broke during AWS authentication. Please copy and paste the AWS key code. " + str(e))
+        #Ensure it exists!
         st.stop()
-    #with st.expander("Click to read more in depth about this data"):
-        #st.write("This will create a better visualization for you")
-    #def load_lottieurl(url: str):
-    #try:
-        #r = requests.get(url)
-        #if r.status_code != 200:
-            #return None
-        #return r.json()
-    except:
-        st.write("Ensure you have loaded everything")
+# Authenticate into google
+
+    try:
+        parquet_file_path = f"{camera_id}_data_{year_month}/predictions.parquet"
+    except Exception as e:
+        st.error(f"That parquet file doesnt exist! Double check gcs")
         st.stop()
-        # load local css file
-    con.close()
-        # use local css file"""
